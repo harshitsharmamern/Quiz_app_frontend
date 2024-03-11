858ce871c7e4bfe3d8fa4e2cddaf2a5548dd8209
harshit_sharma <sharmaharshit769@gmail.com>
2024-03-11 23:58:56 +0530

    redux-remove

--- src/components/user/Home.jsx
+++ src/components/user/Home.jsx
@@ -1,6 +1,5 @@
 import Navbar from '../common/Navbar'
 import React, { useEffect, } from 'react'
-import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import AllQuiz from '../HomeComponent/AllQuiz';
 import LeaderBoard from '../HomeComponent/LeaderBoard';
--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 ///////////
-import { Provider } from 'react-redux';
-// import store from '../src/components/todoapp/Redux/Store';
-// import store from './components/todoapp/Redux/Store';
+
 
 
 
