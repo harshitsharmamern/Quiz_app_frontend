169ab483061b4a5043c462c0d6fd1a2af8c40e67
harshit_sharma <sharmaharshit769@gmail.com>
2024-03-13 17:29:48 +0530

    server-call change

--- src/server_call.js
+++ src/server_call.js
@@ -1,6 +1,6 @@
-const baseUrl = 'http://localhost:5000'
+// const baseUrl = 'http://localhost:5000'
 // const baseUrl= 'https://quiz-app-backend-g0rh.onrender.com'
-// const baseUrl = 'https://quiz-app-backend-g0rh.onrender.com'
+const baseUrl = 'https://quiz-app-backend-g0rh.onrender.com'
 
 export {baseUrl}
 // 'http://localhost:5000'
